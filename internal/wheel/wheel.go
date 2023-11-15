916263d1f6bbb77161722588799518b2d7649341
seiya <ilya_shtak@mail.ru>
2023-11-16 05:54:45 +0700

    wheel and spin

--- internal/wheel/wheel.go
+++ internal/wheel/wheel.go
@@ -1,15 +1,37 @@
 package wheel
 
+import (
+	"math/rand"
+	"time"
+)
+
 type Wheel struct {
 	Num_of_cells int8
-	Cell         Cell
+	Slots        Slot
 }
 
-func (p Wheel) Rotation() {
+func (w Wheel) Spin() float64 {
+	rand.Seed(time.Now().UnixNano())
+	weights := []float64{0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0}
 
+	sum := 0.0
+	for _, weight := range weights {
+		sum += weight
+	}
+
+	value := rand.Float64() * sum
+
+	for _, weight := range weights {
+		value -= weight
+		if value <= 0 {
+			return weight
+		}
+	}
+
+	return weights[len(weights)-1]
 }
 
-type Cell struct {
+type Slot struct {
 	Name   string
 	Image  string
 	Chance int8
